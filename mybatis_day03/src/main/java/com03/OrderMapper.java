7d7a6ab99b2bd74b813a79c3940ec950a42a00ff
姜超 <1527243306@qq.com>
2019-08-19 23:44:58 +0800

    删除 mybatis_day03项目

--- mybatis_day03/src/main/java/com03/OrderMapper.java
+++
@@ -1,5 +0,0 @@
-package com03;
-import java.util.List;
-public interface OrderMapper {
-        public List<Order> fiandAll();
-}
--- mybatis_day03/src/main/java/com03/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com03;
-
-import java.util.List;
-
-public interface UserMapper {
-    public List<User> findAllUser();
-
-    public List<User> findUserAndRoleAll();
-}
